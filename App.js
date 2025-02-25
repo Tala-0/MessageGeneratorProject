b1ba53cd6fe3cc0867569a4fa3ad4fe609cc159d
Sophie Schweyer <s.schweyer@gmail.com>
2025-02-25 16:55:43 +0100

    got started with message generation

--- App.js
+++ App.js
@@ -1,5 +1,56 @@
+const message = new Object();
+message.planets = [
+  "Mars",
+  "Venus",
+  "Neptune",
+  "Mercury",
+  "Saturn",
+  "Jupiter",
+  "Uranus",
+  "Pluto",
+];
+
+message.energies = [
+  "tired",
+  "happy",
+  "fast",
+  "slow",
+  "athletic",
+  "comfy",
+  "crafty",
+  "nurturing",
+];
+
+message.tasks = [
+  "sleep",
+  "party",
+  "take a cold shower",
+  "garden",
+  "go for a walk",
+  "cook",
+  "clean",
+  "invite friends",
+];
+
+function generateMessage() {
+  let pt1 =
+    "The moon is in " + Math.floor(Math.random() * message.planets.length);
+}
+
 // Create a button element
-const button = document.createElement('button')
+const button = document.createElement("button");
 
 // Set the button text to 'Can you click me?'
-button.innerText = 'Can you click me?'
+button.innerText = "Want to know your horoscope for the day?";
+
+button.id = "mainButton";
+
+// Attach the "click" event to your button
+button.addEventListener("click", () => {
+  // When there is a "click"
+  // it shows an alert in the browser
+  alert("Oh, you clicked me!");
+});
+
+document.body.appendChild(button);
+("J");
